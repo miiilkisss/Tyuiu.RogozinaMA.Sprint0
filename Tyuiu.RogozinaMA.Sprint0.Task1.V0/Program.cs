260c14b6ec4892eb16f65da683f04d3dd0a60970
Tyuiu.RogozinaMA@outlook.com <mila.rogo@yandex.ru>
2025-09-18 16:20:03 +0500

    Task1

---
+++ Tyuiu.RogozinaMA.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.RogozinaMA.Sprint0.Task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+                string name = "Milana";
+                int age = 18;
+                Console.WriteLine("Здравствуйте меня зовут " + name + " мне " + age + " лет!");
+                Console.ReadKey();
+
+        }
+    }
+}
